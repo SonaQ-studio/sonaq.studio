c6129b2dbce023951330e9c85508b1fe6ccec8f7
leopold <galileopold7@gmail.com>
2026-07-20 21:26:09 +0300

    Align Yandex Metrika embed with official counter snippet.

--- js/site.js
+++ js/site.js
@@ -7,31 +7,43 @@
  * Google Analytics 4: https://analytics.google.com/ → Measurement ID (G-XXXXXXXX)
  */
 (function () {
-  var ASSET_V = "20260722";
+  var ASSET_V = "20260723";
 
-  // === Аналитика ===
+  // === Аналитика (официальный код Метрики) ===
   var YANDEX_METRIKA_ID = "110894233";
   var GA_MEASUREMENT_ID = ""; // Google не подключаем
 
   function loadYandexMetrika(id) {
     if (!id) return;
-    window.ym =
-      window.ym ||
-      function () {
-        (window.ym.a = window.ym.a || []).push(arguments);
-      };
-    window.ym.l = 1 * new Date();
-    var s = document.createElement("script");
-    s.async = true;
-    s.src = "https://mc.yandex.ru/metrika/tag.js";
-    document.head.appendChild(s);
+    // Как в кабинете Яндекс.Метрики
+    (function (m, e, t, r, i, k, a) {
+      m[i] =
+        m[i] ||
+        function () {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) return;
+      }
+      k = e.createElement(t);
+      a = e.getElementsByTagName(t)[0];
+      k.async = 1;
+      k.src = r;
+      a.parentNode.insertBefore(k, a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=" + id, "ym");
+
     window.ym(Number(id), "init", {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
+      ssr: true,
       webvisor: true,
+      clickmap: true,
+      ecommerce: "dataLayer",
+      referrer: document.referrer,
+      url: location.href,
+      accurateTrackBounce: true,
+      trackLinks: true,
     });
-    // noscript pixel
+
     var noscript = document.createElement("noscript");
     noscript.innerHTML =
       '<div><img src="https://mc.yandex.ru/watch/' +
